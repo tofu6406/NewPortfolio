1b31c91c5608aa54c1f743e9c5431f10f1660f1a
tofu6406 <131725612+tofu6406@users.noreply.github.com>
2024-10-31 15:26:31 +0900

    Add files via upload

---
+++ works/main.js
@@ -0,0 +1,15 @@
+console.log("main.js!!");
+
+
+$(document).ready(function() {
+    // ページロード後、worksとanimateTextが2秒表示される
+    setTimeout(function() {
+        // introText全体が上に移動するアニメーション
+        $('#introText').addClass('show');
+        
+        // 1秒遅らせてwebPageInfoが下から出現する
+        setTimeout(function() {
+            $('#webPageInfo').addClass('show'), $('#introText').css('margin-bottom', '-150px');;
+        }, 1000); // 1秒遅延
+    }, 2500); // 2秒後にworksとanimateTextが上に移動
+});
